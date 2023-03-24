c7aec3d0bab9574ca0ab78b23dc8954f5801521e
JamesThang996 <baokieuha@gmail.com>
2023-03-24 22:48:41 +0700

    Initial Commit

---
+++ BurgerMe/BurgerMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerMeApp.swift
+//  BurgerMe
+//
+//  Created by James Thang on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BurgerMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
